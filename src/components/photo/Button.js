4f3e432c4ec9eb2352584578ab380ef9e574bae9
dimas <dmitry.shevchenko@link4.tech>
2025-08-07 21:13:56 +0300

    Renamed vars

--- src/components/photo/Button.js
+++ src/components/photo/Button.js
@@ -1,7 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Button = () => {
     const [buttonText, setButtonText] = useState("Photo Button");
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setButtonText("Click!");
+        }, 5000);
+
+        return () => clearTimeout(timer);
+    }, []);
 
     const handleClick = () => {
         setButtonText("Photo Button Clicked");

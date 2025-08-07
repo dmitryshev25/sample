18c3fd378ae80329b164f6dad8eac9eee7ccc641
dimas <dmitry.shevchenko@link4.tech>
2025-08-07 20:51:29 +0300

    Added useState

--- src/components/paul/Button.js
+++ src/components/paul/Button.js
@@ -1,9 +1,17 @@
+import React, { useState } from 'react';
+
 const Button = () => {
+    const [buttonText, setButtonText] = useState("John Button");
+
+    const handleClick = () => {
+        setButtonText("John Button Clicked");
+    };
+
     return (
-        <button>
-            John Button
+        <button onClick={handleClick}>
+            {buttonText}
         </button>
     );
-}
+};
 
 export default Button;
